ff2aea701294768e3f0d1e8ea0508c99fab83d4f
santa-sukitoku <santanaruse@gmail.com>
2019-11-28 16:51:34 +0900

    Add the Python tool for recording.

---
+++ sampling.py
@@ -0,0 +1,37 @@
+import pyaudio
+import wave
+
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = 'sample.wav'
+iDeviceIndex = 0
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 2 ** 11
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    input_device_index=iDeviceIndex,
+                    frames_per_buffer=CHUNK)
+
+input('Press Any key to record for 5 seconds. Please tell me your name.')
+print('Recording...')
+frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print('Finished recording!')
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
